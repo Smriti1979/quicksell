451a079fe9a45a17576ba2830661db71445204d3
Smriti <smriti12sci.nvm@gmail.com>
2024-11-12 11:01:08 +0530

    icon issue fix

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -8,6 +8,16 @@ import BacklogIcon from '../../assets/Backlog.svg';
 import CancelledIcon from '../../assets/Cancelled.svg';
 import Add from '../../assets/add.svg';
 import Options from '../../assets/3 dot menu.svg';
+import High from '../../assets/high-priority.svg';
+import Urgent from '../../assets/urgent-priority.svg';
+import Low from '../../assets/low-priority.svg';
+import NoPriority from '../../assets/No-priority.svg';
+import Medium from '../../assets/medium-priority.svg';
+import Anoop from '../../assets/Anoop.svg';
+import Yogesh from '../../assets/yogesh.svg';
+import Suresh from '../../assets/suresh.svg';
+import Shankar from '../../assets/shankar.svg';
+import Ramesh from '../../assets/ramesh.svg';
 
 function Column({ title, tickets, users }) {
   console.log('Column:', title, tickets);
@@ -17,6 +27,16 @@ function Column({ title, tickets, users }) {
     'Done': <img src={DoneIcon} alt="Done" className="column-icon" />,
     'Backlog': <img src={BacklogIcon} alt="Backlog" className="column-icon" />,
     'Cancelled': <img src={CancelledIcon} alt="Cancelled" className="column-icon" />,
+    'High': <img src={High} alt="High Priority" className="column-icon" />,
+    'Urgent': <img src={Urgent} alt="Urgent Priority" className="column-icon" />,
+    'Low': <img src={Low} alt="Low Priority" className="column-icon" />,
+    'No priority': <img src={NoPriority} alt="No Priority" className="column-icon" />,
+    'Medium': <img src={Medium} alt="Medium Priority" className="column-icon" />,
+    'Anoop sharma': <img src={Anoop} alt="Anoop" className="column-icon" />,
+    'Yogesh': <img src={Yogesh} alt="Yogesh" className="column-icon" />,
+    'Suresh': <img src={Suresh} alt="Suresh" className="column-icon" />,
+    'Shankar Kumar': <img src={Shankar} alt="Shankar" className="column-icon" />,
+    'Ramesh': <img src={Ramesh} alt="Ramesh" className="column-icon" />
   };
 
   const columnIcon = columnIcons[title] || <span className="column-icon">[{title}]</span>;
